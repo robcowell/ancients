afbb095a59f134cfda4f7447bcee8bb2de2ccfe7
MSI\Rob Cowell <bossman@demografica.org>
2019-12-04 09:17:44 +0000

    Selector image conversion

---
+++ 1bpl.py
@@ -0,0 +1,33 @@
+PIC_DIR="./"
+
+def load_bitmaps(fnames, outfile):
+    import struct
+    # Write out the offset table first
+    for i in range(0, len(fnames)):
+        offset = 4*len(fnames) + i * 8000
+        outfile.write(struct.pack(">I", offset))
+
+    # Now convert the images
+    # Be sure to specify the correct image dimensions for x/y
+    for fname in fnames:
+        from PIL import Image
+        original = Image.open(PIC_DIR + fname)
+
+        bits = bytearray()
+        for y in range(0,384):
+            acc = 0
+            for x in range(0,120):
+                p = original.getpixel((x,y)) & 1
+                #assert p < 2, "p is %d" % p
+                acc <<= 1
+                acc |= p
+                if (x&7 == 7):
+                    bits.append(acc)
+                    acc = 0
+        outfile.write(bits)
+
+fnames = []
+fnames.append("selector.png")
+outfile = open("select.dat", "wb")
+load_bitmaps(fnames, outfile)
+outfile.close()
